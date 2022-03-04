319dcadf72561fd021c67f64fc50c80343080685
EngelsZ10 <Engelsmpz@gmail.com>
2022-03-03 23:20:13 -0600

    add to apliacaión de convoluciones bootstrap

--- hello/static/scripts/script.js
+++ hello/static/scripts/script.js
@@ -2,10 +2,18 @@ function myFunction() {
     document.getElementById("p1").innerHTML = document.getElementById("lname").value;
 }
 
+function valida() {
+    if (document.forms["form"]["lname"].value == "") {
+        alert("no puedes mandar un elemento vacio");
+        return false;
+    }
+
+}
+
 document.getElementById("bdy").style.backgroundImage = "url('../static/assets/default.jpg')";
-//document.getElementById("i").href = "";
-//document.getElementById("c").href = "/conv";
-//document.getElementById("a").href = "/app";
+//document.getElementById("i").href = "index.html";
+//document.getElementById("c").href = "Convolution.html";
+//document.getElementById("a").href = "Aplication.html";
 
 
 document.getElementById("GC").onmouseover = function() {
